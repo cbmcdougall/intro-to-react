8090ab553fc12288d9b45c56f2f045db8f7b94f4
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-11-02 17:49:55 +0000

    add toggle button for card

--- src/components/Album/index.jsx
+++ src/components/Album/index.jsx
@@ -5,20 +5,26 @@ import { Songs } from './Songs';
 import './album.css';
 
 export const Album = ({ size, image, title, year, songs }) => {
-    const [isLarge, setLarge] = useState(false);
+    // Set initial state according to specified size in props
+    const checkLarge = size==="large" ? true : false
+    const [isLarge, setLarge] = checkLarge ? useState(true) : useState(false);
+    // Callback function to toggle the large expanded album card
     const toggleLarge = () => setLarge((prevIsLarge) => !prevIsLarge)
-    let heading=<h3><FaveButton />{title} ({year})</h3>;
-    if (size==="small"){ heading = <><h3><FaveButton />{title}</h3><h4>({year})</h4></> }
-    // Create see more button to assign onclick event to
+    // General heading
+    let heading=<h3>{title} ({year})</h3>;
+    // Separate album and year on the small card to allow for better styling
+    if (size==="small"){ heading = <><h3>{title}</h3><h4>({year})</h4></> }
     return (
         <div  role="menuitem" className={isLarge ? 'album large' : `album ${size}`}>
-            <div onClick={toggleLarge} className="image-container">
+            <div className="image-container">
                 <img width="100%" src={image || placeholder} alt={`${title} album cover`}/>
             </div>
             <div className="album-container">
                 {heading}
                 <Songs songs={songs}/>
             </div>
+            <FaveButton />
+            <span className="card-toggle" role="button" onClick={toggleLarge}>{isLarge ? <>&times;</> : "View Album"}</span>
         </div>
     )
 }
