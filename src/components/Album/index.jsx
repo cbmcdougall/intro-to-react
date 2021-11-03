5d6f82d9123b175545527e85385439c9176ecd8d
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-11-03 13:28:24 +0000

    reconfigure to retrieve album image and songlist from last.fm API

--- src/components/Album/index.jsx
+++ src/components/Album/index.jsx
@@ -1,18 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaveButton } from '../FaveButton';
 import { Songs } from './Songs';
+import axios from 'axios';
 import './album.css';
 
-export const Album = ({ size, image, title, year, songs }) => {
+export const Album = ({ size, title, year }) => {
+    // Get album image, songs
+    const [ image, setImage ] = useState();
+    const [ songs, setSongs ] = useState([]);
+    useEffect(() => {
+        const getAlbumInfo = async () => {
+            const { data } = await axios.get(`https://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=fa6c736ed5fc986ba1d168fd665b2a3f&artist=Amon%20Amarth&album=${title}&format=json`);
+            const imageUrl = data.album.image[3]["#text"];
+            setImage(imageUrl);
+            const tracks = data.album.tracks.track.map(t => t.name)
+            setSongs(tracks);
+        }
+        getAlbumInfo();
+    }, [])
+
     // Set initial state according to specified size in props
     const checkLarge = size==="large" ? true : false
-    const [isLarge, setLarge] = checkLarge ? useState(true) : useState(false);
+    const [isLarge, setLarge] = useState(checkLarge)
+
     // Callback function to toggle the large expanded album card
     const toggleLarge = () => setLarge((prevIsLarge) => !prevIsLarge)
+
     // General heading
     let heading=<h3>{title} ({year})</h3>;
     // Separate album and year on the small card to allow for better styling
     if (size==="small"){ heading = <><h3>{title}</h3><h4>({year})</h4></> }
+
     return (
         <div  role="menuitem" className={isLarge ? 'album large' : `album ${size}`}>
             <div className="image-container">
