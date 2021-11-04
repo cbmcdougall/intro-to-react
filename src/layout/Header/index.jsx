c8dee893ed38a9ef0f2ab3ec87d239797d7adb5f
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-11-04 11:09:03 +0000

    add image, styling

--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import logo from './AmonAmarth.webp';
+import './header.css';
 
 export const Header = () => {
     return(
         <header className="header">
+            <img width="90px" height="100px" src={logo} alt="Band Logo" />
             <h1>Amon Amarth - <em>Melodic Death Metal</em></h1>
-            <nav>
-                <NavLink exact to='/'><button>Home</button></NavLink>
-                <NavLink to='/StudioAlbums'><button>Studio Albums</button></NavLink>
+            <nav id="navbar">
+                <NavLink className="navlink" exact to='/'><button className="navbutton">Home</button></NavLink>
+                <NavLink className="navlink" to='/StudioAlbums'><button className="navbutton">Studio Albums</button></NavLink>
                 {/* Back Button */}
             </nav>
         </header>
