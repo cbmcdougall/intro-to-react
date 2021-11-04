79204e7e6282ba5bcbd233f9c35c31bf5bf1c08e
cbmcdougall <c_mcdougall97@yahoo.co.uk>
2021-11-04 12:49:38 +0000

    slight tweaks

--- src/components/Albums/index.jsx
+++ src/components/Albums/index.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 import { Album } from '..';
 import './albums.css';
 
-const albums = [
-    {title: "Berserker", year: 2019},
-    {title: "Jomsviking", year: 2016},
-    {title: "Deceiver of the Gods", year: 2013},
-    {title: "Surtur Rising", year: 2011},
-    {title: "Twilight Of The Thunder God", year: 2008},
-    {title: "With Oden On Our Side", year: 2006},
-    {title: "Fate of Norns", year: 2004},
-    {title: "Versus The World", year: 2002},
-    {title: "The Crusher", year: 2001},
-    {title: "The Avenger", year: 1999},
-    {title: "Once Sent From The Golden Hall", year: 1998}
-]
 
 export const Albums = ({isMobile}) => {
+    const albums = [
+        {title: "Berserker", year: 2019},
+        {title: "Jomsviking", year: 2016},
+        {title: "Deceiver of the Gods", year: 2013},
+        {title: "Surtur Rising", year: 2011},
+        {title: "Twilight Of The Thunder God", year: 2008},
+        {title: "With Oden On Our Side", year: 2006},
+        {title: "Fate of Norns", year: 2004},
+        {title: "Versus The World", year: 2002},
+        {title: "The Crusher", year: 2001},
+        {title: "The Avenger", year: 1999},
+        {title: "Once Sent From The Golden Hall", year: 1998}
+    ]
     const size = isMobile ? "small" : "medium";
     const renderAlbums = () => albums.map((a, i) => <Album key={i} size={size} title={a.title} year={a.year}/>);
 
--- src/pages/StudioAlbums/index.jsx
+++ src/pages/StudioAlbums/index.jsx
@@ -5,19 +5,15 @@ export const StudioAlbums = () => {
     const [isMobile, setIsMobile] = useState(window.innerWidth < 800);
 
     useEffect(() => {
-        // Define isMounted to skip setting state if component is unmounted
-        let isMounted = true;
-        
         const handleResize = () => {
             const ismobile = window.innerWidth < 800;
-            if (isMounted && ismobile !== isMobile) setIsMobile(ismobile);
+            if (ismobile !== isMobile) setIsMobile(ismobile);
         }
         
         window.addEventListener("resize", handleResize);
         
-        return function cleanup() {
-            isMounted = false;
-        }
+        return () => window.removeEventListener("resize", handleResize);
+
     }, [isMobile])
 
     return <Albums isMobile={isMobile}/>
